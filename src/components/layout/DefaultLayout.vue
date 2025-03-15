13d55b228f53bfae2d06686cf358d7b2901c931a
Luka Vilagoš <luka.vilagos@gmail.com>
2025-03-15 20:05:52 +0100

    Fixed Navbar

--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -3,8 +3,8 @@ import Navbar from './Navbar.vue';
 </script>
 
 <template>
-    <div class="grid grid-cols-[auto_1fr] h-screen">
-        <div class="flex p-2">
+    <div>
+        <div class="p-2">
             <Navbar />
         </div>
         <div class=" overflow-y-scroll scroll-smooth no-scrollbar">
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,61 +1,60 @@
 <script setup lang="ts">
-import { Menu, Avatar } from "primevue";
+import { Avatar, Menu, Menubar } from "primevue";
 import { ref } from "vue";
 import { useAuthenticatedUser, useAuthGuard } from "../../composables/useAuthGuard";
 import type { MenuItem } from "primevue/menuitem";
+import { useTypedRouter } from "../../composables/useTypedRouter";
 
 const { signOut } = useAuthGuard();
 const user = useAuthenticatedUser();
+const router = useTypedRouter();
 
 const items = ref<MenuItem[]>([
     {
         separator: true,
     },
     {
-        items: [
-            {
-                label: 'Home',
-                icon: 'pi pi-home',
-            },
-        ]
-    },
-    {
-        label: 'Profile',
-        items: [
-            {
-                label: 'Settings',
-                icon: 'pi pi-cog',
-            },
-            {
-                label: 'Logout',
-                icon: 'pi pi-sign-out',
-                command: () => signOut()
-
-            }
-        ]
+        label: 'Home',
+        icon: 'pi pi-home',
+        command: () => router.typedPush({ name: "Home", params: { userId: user.uid } })
     },
     {
         separator: true,
     }
 ]);
 
+const profileMenu = ref();
+const profileMenuItems = ref([
+    {
+        label: 'Profile',
+        icon: 'pi pi-user',
+    },
+    {
+        label: 'Sign Out',
+        icon: 'pi pi-sign-out',
+        command: () => signOut()
+    }
+]);
+
+const toggle = (event: any) => {
+    profileMenu.value.toggle(event);
+};
+
+
 </script>
 
 <template>
-    <div class="card flex justify-center">
-        <Menu :model="items" class="w-full md:w-60">
-            <template #start>
-                <h2 class="text-center py-4 font-extrabold text-teal-600">EU HEALTH SYSTEM</h2>
-            </template>
-            <template #end>
-                <div class="flex items-end h-full">
-                    <button
-                        class="overflow-hidden w-full border-0 bg-transparent flex items-center py-2 px-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
-                        <Avatar :image="user.photoURL as string" class="mr-2" shape="circle" />
-                        <span>{{ user.displayName }}</span>
-                    </button>
-                </div>
-            </template>
-        </Menu>
-    </div>
+    <Menubar :model="items" class="w-full">
+        <template #start>
+            <h2 class="text-center py-4 font-extrabold text-teal-600">EU HEALTH SYSTEM</h2>
+        </template>
+        <template #end>
+            <button @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"
+                class="overflow-hidden w-full border-0 bg-transparent flex items-center py-2 px-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
+                <Avatar :image="user.photoURL as string" class="mr-2" shape="circle" />
+                <span>{{ user.displayName }}</span>
+            </button>
+            <Menu ref="profileMenu" id="overlay_menu" :model="profileMenuItems" :popup="true" />
+        </template>
+    </Menubar>
 </template>
